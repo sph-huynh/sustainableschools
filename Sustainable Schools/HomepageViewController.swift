c61b43d448ae4414cd79acf94d508f862c33ccd3
sph-huynh <shuy14@student.monash.edu>
2017-05-02 18:04:06 +1000

    Initial Commit

---
+++ Sustainable Schools/HomepageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomepageViewController.swift
+//  Sustainable Schools
+//
+//  Created by Sophia Huynh on 2/5/17.
+//  Copyright © 2017 Sophia Huynh. All rights reserved.
+//
+
+import UIKit
+
+class HomepageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
