4a12e09837c5a9897465926484aa7eac811c7dda
sph-huynh <shuy14@student.monash.edu>
2017-05-04 17:31:02 +1000

    adding interface

---
+++ Sustainable Schools/QuizViewController.swift
@@ -0,0 +1,35 @@
+//
+//  QuizViewController.swift
+//  Sustainable Schools
+//
+//  Created by Admin on 04/05/2017.
+//  Copyright © 2017 Sophia Huynh. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
