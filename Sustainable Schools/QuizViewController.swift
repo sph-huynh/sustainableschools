8093a418d697bc858414dfafe79b997f97ddb22a
sph-huynh <shuy14@student.monash.edu>
2017-05-06 18:39:56 +1000

    looks terrible but a quiz is working

--- Sustainable Schools/QuizViewController.swift
+++ Sustainable Schools/QuizViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class QuizViewController: UIViewController {
     
-    let question = ["How many Sustainable Development Goals are there?", "What is Goal 1?", "What is Goal 3?"]
-    let answers = [[17, 19, 8, 4],["Producing less waste", "Using more solar", "Ensuring everybody has access to water", "End all forms of poverty everywhere"], ["Using clean and sustainable energy", "Building sustainable cities", "Using less energy", "Promoting gender equality"]]
+    let questions = ["How many Sustainable Development Goals are there?", "What is Goal 1?", "What is Goal 3?"]
+    let answers = [["17", "19", "8", "4"],["Producing less waste", "Using more solar", "Ensuring everybody has access to water", "End all forms of poverty everywhere"], ["Using clean and sustainable energy", "Building sustainable cities", "Using less energy", "Promoting gender equality"]]
     
-    let currentQuestion = 0
-    let currentAnswer = 0
+    var currentQuestion = 0
+    var correctAnswerPlacement:UInt32 = 0
 
-    @IBOutlet weak var answer1Button: UIButton!
-    @IBOutlet weak var answer2Button: UIButton!
-    @IBOutlet weak var answer3Button: UIButton!
-    @IBOutlet weak var answer4Button: UIButton!
+
+    @IBAction func AnswerButton(_ sender: AnyObject) {
+        if (sender.tag == Int(correctAnswerPlacement)){
+            print ("correct")
+        }
+        else{
+            print("wrong")
+        }
+        
+        if (currentQuestion != questions.count){
+            displayQuestion()
+        }
+        
+    }
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+
+    
+    override func viewDidAppear(_ animated: Bool) {
+        displayQuestion()
+        
+    }
+    
+    // Question function
+    func displayQuestion(){
+        questionLabel.text = questions[currentQuestion]
+        
+        correctAnswerPlacement = arc4random_uniform(4)+1
+        
+        var button:UIButton = UIButton()
+        
+        //to jump over the correct answer, we store this index
+        var incorrectAnswerIndex = 1
+        
+        // we access each button by creating a new button with the tags set
+        for j in 1...4{
+            button = view.viewWithTag(j) as! UIButton
+            
+            if (j == Int(correctAnswerPlacement)){
+                button.setTitle(answers[currentQuestion][0], for: .normal)
+            }
+            else {
+                button.setTitle(answers[currentQuestion][incorrectAnswerIndex], for: .normal)
+                incorrectAnswerIndex = 2
+            }
+            
+            
+        }
+        currentQuestion += 1
+
+    }
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
