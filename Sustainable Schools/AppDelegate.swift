681d45f7198b274249e0a528fb6229fff0a62443
sph-huynh <shuy14@student.monash.edu>
2017-05-15 04:35:59 +1000

    I have buttons that work now

--- Sustainable Schools/AppDelegate.swift
+++ Sustainable Schools/AppDelegate.swift
@@ -15,50 +15,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-    
-    
-    public class func getSustainableDataFromFileWithSuccess(success: @escaping ((_ data: Data) -> Void)) {
-        DispatchQueue.global(qos: .background).async {
-            let filePath = Bundle.main.path(forResource: "topapps", ofType:"json")
-            let data = try! Data(contentsOf: URL(fileURLWithPath:filePath!), options: .uncached)
-            
-            success(data)
-        }
-    }
-    
-    public class func loadDataFromURL(url: URL, completion: @escaping (_ data: Data?, _ error: Error?) -> Void) {
-        let loadDataTask = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            if let _ = error {
-                completion(nil, error)
-            } else if let response = response as? HTTPURLResponse {
-                if response.statusCode != 200 {
-                    let statusError = NSError(domain: "com.raywenderlich",
-                                              code: response.statusCode,
-                                              userInfo: [NSLocalizedDescriptionKey: "HTTP status code has unexpected value."])
-                    completion(nil, statusError)
-                } else {
-                    completion(data, nil)
-                }
-            }
-        }
-        loadDataTask.resume()
-    }
-    
-    public class func getSustainableDataFromNetWithSuccess(success: @escaping ((_ data: Data) -> Void)) {
-        //1
-        let quizURL = "https://github.com/sph-huynh/sustainable-schools-quiz/blob/master/data.json"
-
-        loadDataFromURL(url: URL(string: quizURL)!) { (data, error) -> Void in
-            //2
-            if let data = data {
-                //3
-                success(data)
-            }
-        }
-    }
-
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         FIRApp.configure()
--- Sustainable Schools/HomepageViewController.swift
+++ Sustainable Schools/HomepageViewController.swift
@@ -8,11 +8,50 @@
 
 import UIKit
 import QuartzCore
+import FirebaseAuth
 
 
 class HomepageViewController: UIViewController {
+    
+    var emailInput: String = "email.com"
+    var passwordInput: String = "password"
+    
+    @IBOutlet weak var loginButton: UIButton!
 
-    @IBOutlet weak var LoginButton: UIButton!
+    @IBOutlet weak var emailLabel: UITextField!
+    @IBOutlet weak var passwordLabel: UITextField!
+    @IBOutlet weak var registerToggle: UIButton!
+    
+    @IBAction func authUser(){
+        emailInput = emailLabel.text!
+        passwordInput = passwordLabel.text!
+        loginUser(email: emailInput, password: passwordInput)
+    }
+    
+    @IBAction func registerToggleButton(){
+        registerUser(email: emailInput, password: passwordInput)
+    }
+    
+    func loginUser(email: String, password: String){
+        print("I log you in now")
+//        FIRAuth.auth()?.signIn(withEmail: email, password: password) { (user, error) in
+//            // ...
+//        }
+    }
+    
+    
+
+    
+    func registerUser(email: String, password: String){
+        print("I register user now")
+
+//        FIRAuth.auth()?.createUser(withEmail: email, password: password) { (user, error) in
+//            // ...
+//        }
+
+    }
+    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
